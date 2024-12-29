528ccefec6c7be3bc2ca50d6f5b2560f9717bc8d
Louis Escher <louisescher@proton.me>
2024-12-29 19:45:45 +0100

    add debug console.log

--- src/pages/styles/giscus.css.ts
+++ src/pages/styles/giscus.css.ts
@@ -5,6 +5,8 @@ import css from '../../styles/giscus.css?raw';
 export async function ALL() {
   const res = new Response(css);
 
+  console.log("hrhrtr")
+
   res.headers.set('Content-Type', 'text/css');
   res.headers.set('Access-Control-Allow-Origin', '*');
   res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
