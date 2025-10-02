bd54061e387d7848dfb27f16b69e03b8ff4c3ddd
tpodsosony <talpods6@gmail.com>
2025-10-02 15:28:17 -0500

    Initial Commit

--- astro.config.ts
+++ astro.config.ts
@@ -9,17 +9,6 @@ import spectre from './package/src';
 import node from '@astrojs/node';
 import { spectreDark } from './src/ec-theme';
 
-const {
-  GISCUS_REPO,
-  GISCUS_REPO_ID,
-  GISCUS_CATEGORY,
-  GISCUS_CATEGORY_ID,
-  GISCUS_MAPPING,
-  GISCUS_STRICT,
-  GISCUS_REACTIONS_ENABLED,
-  GISCUS_EMIT_METADATA,
-  GISCUS_LANG
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
 // https://astro.build/config
 const config = defineConfig({
@@ -32,11 +21,11 @@ const config = defineConfig({
     mdx(),
     sitemap(),
     spectre({
-      name: 'Spectre',
+      name: 'Tal Podsosony',
       openGraph: {
         home: {
           title: 'Spectre',
-          description: 'A minimalistic theme for Astro.'
+          description: 'A display of the work of Tal Podsosony'
         },
         blog: {
           title: 'Blog',
@@ -46,17 +35,7 @@ const config = defineConfig({
           title: 'Projects'
         }
       },
-      giscus: {
-        repository: GISCUS_REPO,
-        repositoryId: GISCUS_REPO_ID,
-        category: GISCUS_CATEGORY,
-        categoryId: GISCUS_CATEGORY_ID,
-        mapping: GISCUS_MAPPING as any,
-        strict: GISCUS_STRICT === "true",
-        reactionsEnabled: GISCUS_REACTIONS_ENABLED === "true",
-        emitMetadata: GISCUS_EMIT_METADATA === "true",
-        lang: GISCUS_LANG,
-      }
+      
     })
   ],
   adapter: node({
